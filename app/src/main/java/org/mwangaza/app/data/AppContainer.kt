63bb8ec819261e0bd874619004b29ccc98b19a65
sammyotsieno-cloud <sammyotsieno@gmail.com>
2026-09-15 06:48:52 +0300

    feat: wire inventory valuation service into app container

--- app/src/main/java/org/mwangaza/app/data/AppContainer.kt
+++ app/src/main/java/org/mwangaza/app/data/AppContainer.kt
@@ -11,6 +11,7 @@ import core.domain.persistence.StockAllocationDao
 import core.domain.persistence.StockBatchDao
 import core.domain.persistence.StockMovementDao
 import core.domain.receiving.GoodsReceiptPersistenceService
+import core.domain.reporting.InventoryValuationService
 
 class AppContainer(context: Context) {
     val database: CoreDatabase = CoreDatabase.getInstance(context)
@@ -23,6 +24,12 @@ class AppContainer(context: Context) {
     val stockAllocationDao: StockAllocationDao = database.stockAllocationDao()
     val saleDao: SaleDao = database.saleDao()
 
-    val receivingService: GoodsReceiptPersistenceService = GoodsReceiptPersistenceService(database)
-    val consumptionService: ConsumptionService = ConsumptionService(database)
+    val receivingService: GoodsReceiptPersistenceService =
+        GoodsReceiptPersistenceService(database)
+
+    val consumptionService: ConsumptionService =
+        ConsumptionService(database)
+
+    val inventoryValuationService: InventoryValuationService =
+        InventoryValuationService(inventoryCostLayerDao)
 }
